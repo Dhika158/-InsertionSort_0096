2daafd43d30f0711e3d5efe39f9790530b6d5dc4
Dhika158 <andhikanurjamil7@gmail.com>
2025-03-23 16:25:59 +0700

    Memanggil variable inputan n

--- InsertionSort1.cpp
+++ InsertionSort1.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
- 
+
+void input(){
+    while (true){
+        cout << "Memasukkan Jumlah Data pada Array : ";
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20 ){
+            break;
+        }
+    }
+}
